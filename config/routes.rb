19ce20a03a31e7b526579b06c6e2faea69d92398
Kinaan19 <k.alhatem@hotmail.com>
2019-09-23 17:17:34 +0200

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   
-  delete '/logout',  to: 'sessions#destroy'
   get  '/logout',    to: 'sessions#destroy'
-  post '/login',     to: 'sessions#create'
   get  '/login',     to: 'sessions#new'
   get  '/help',      to: 'static_pages#help'
   get  '/about',     to: 'static_pages#about'
   get  '/contact',   to: 'static_pages#contact'
   get  '/signup',    to: 'users#new'
+  post '/login',     to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
+  resources :account_activations, only: [:edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
