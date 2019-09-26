61b6addd9f8d26bea93fd0068e03facac765e806
Kinaan19 <k.alhatem@hotmail.com>
2019-09-26 16:00:47 +0200

    Add password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
   host = 'localhost:3000' # Don't use this literally; use your local dev host
   # instead
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -57,4 +57,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  
 end
