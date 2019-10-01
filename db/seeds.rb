0898d0255c0a4d82109af72dc4e754342e103de4
Kinaan19 <k.alhatem@hotmail.com>
2019-10-01 11:49:20 +0200

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,8 @@ User.create!(
   )
 end
   
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
